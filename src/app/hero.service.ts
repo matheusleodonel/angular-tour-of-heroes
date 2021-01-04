2dd2a1022a2ff566a13f08a04a7d419f299eaebc
Matheus Leodonel <matheusleodonel17@gmail.com>
2021-01-04 17:31:42 -0300

    Adicionando rotas (passo 5 tutorial)

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,15 +4,20 @@ import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
+
 export class HeroService {
+  
+  constructor(private messageService: MessageService) { }
+  
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes'); //Mandando mensagem depois de buscar os heróis 
     return of (HEROES);
   }
-  constructor(private messageService: MessageService) { }
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 }
 
 
